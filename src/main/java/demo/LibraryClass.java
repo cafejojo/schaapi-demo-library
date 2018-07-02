25bbc200c3626933830a3d62015c487692a6bb8e
Georgios Andreadis <g.andreadis@hetnet.nl>
2018-07-02 15:12:38 +0200

    Update LibraryClass.java

--- src/main/java/demo/LibraryClass.java
+++ src/main/java/demo/LibraryClass.java
@@ -3,9 +3,12 @@ package demo;
 public final class LibraryClass {
   private boolean initialized = false;
   private int initValue = 10;
+  
+  public void initialize() {
+    initialized = true;
+  }
 
   public int foo() {
-    initialized = true;
     return initValue;
   }
 
