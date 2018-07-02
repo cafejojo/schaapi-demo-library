c596bdef27cb92ec5f464326fa06da872eb95b56
Casper Boone <mail@casperboone.nl>
2018-07-02 13:20:02 +0200

    Update LibraryClass.java

--- src/main/java/demo/LibraryClass.java
+++ src/main/java/demo/LibraryClass.java
@@ -3,12 +3,9 @@ package demo;
 public final class LibraryClass {
   private boolean initialized = false;
   private int initValue = 10;
-  
-  public void initialize() {
-    initialized = true;
-  }
 
   public int foo() {
+    initialized = true;
     return initValue;
   }
 
