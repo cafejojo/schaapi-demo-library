8ba4f7695ddd797e900211489d7e48136615862b
Casper Boone <mail@casperboone.nl>
2018-07-02 13:10:20 +0200

    Update DemoLibrary.java

--- src/main/java/demo/DemoLibrary.java
+++ src/main/java/demo/DemoLibrary.java
@@ -3,18 +3,21 @@ package demo;
 public final class LibraryClass {
   private boolean initalized = false;
   private int initValue = 10;
-
+  
+  public void initialize() {
+    initalized = true;
+  }
+  
   public int foo() {
-    initialized = true;
     return initValue;
   }
-
+  
   public double bar(int x) {
     int divisor = x;
-    if (!initialized) {
+    if (!initalized) {
       divisor = x - initValue;
     }
-
+    
     return 100.0 / divisor;
   }
 }
